9173e2b3a68fe2d5956ede3eb4243e1196a30a82
Paul Roetzer <paul.roetzer@uni-bonn.de>
2025-08-26 10:59:52 +0200

    Add python wrappers

--- pythonbinding/pythonwrappers.cpp
+++ pythonbinding/pythonwrappers.cpp
@@ -1,4 +1,4 @@
-#include "cppsrc/my_cpp_code.hpp"
+#include "src/gco_shape_matching.hpp"
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 #include <pybind11/eigen.h>
@@ -12,12 +12,10 @@ using namespace pybind11::literals;  // NOLINT
 PYBIND11_MODULE(sm_gco, handle) {
     handle.doc() = "Python Wrappers for shape matching with gco functions";
 
-    // how to wrap functions, easy huh? :D
-    handle.def("myfunc0", &mycppcode::myfunc0);
-    handle.def("myfunc1", &mycppcode::myfunc1);
 
     // how to wrap classes, also easy *_* (every class function has to be defined seperatly tho :/ )
-    py::class_<mycppcode::MyClass, std::shared_ptr<mycppcode::MyClass>> myclass(handle, "MyClass");
-    myclass.def(py::init<std::string>());
-    myclass.def("print_name", &mycppcode::MyClass::printName);
+    py::class_<smgco::GCOSM, std::shared_ptr<smgco::GCOSM>> myclass(handle, "GCOSM");
+    myclass.def(py::init<const Eigen::MatrixXd, const Eigen::MatrixXi, const Eigen::MatrixXd, const Eigen::MatrixXi, const Eigen::MatrixXd>());
+    myclass.def("point_wise", &smgco::GCOSM::pointWise);
+    myclass.def("set_data_weight", &smgco::GCOSM::setDataWeight);
 }
