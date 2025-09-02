810ca767e6dc795f308bf1e72f3be6130a59a86b
Paul Roetzer <paul.roetzer@uni-bonn.de>
2025-09-02 10:32:08 +0200

    Add enum wrappers

--- pythonbinding/pythonwrappers.cpp
+++ pythonbinding/pythonwrappers.cpp
@@ -11,7 +11,7 @@ using namespace pybind11::literals;  // NOLINT
 
 PYBIND11_MODULE(sm_gco, handle) {
     handle.doc() = "Python Wrappers for shape matching with gco functions";
-
+    using namespace smgco;
 
     // how to wrap classes, also easy *_* (every class function has to be defined seperatly tho :/ )
     py::class_<smgco::GCOSM, std::shared_ptr<smgco::GCOSM>> myclass(handle, "GCOSM");
@@ -20,4 +20,13 @@ PYBIND11_MODULE(sm_gco, handle) {
     myclass.def("triangle_wise", &smgco::GCOSM::triangleWise);
     myclass.def("set_data_weight", &smgco::GCOSM::setDataWeight);
     myclass.def("set_max_iter", &smgco::GCOSM::setMaxIter);
+
+
+    py::enum_<COST_MODE>(handle, "COST_MODE")
+            .value("SINGLE_LABLE_SPACE_L2", SINGLE_LABLE_SPACE_L2)
+            .value("MULTIPLE_LABLE_SPACE_L2", MULTIPLE_LABLE_SPACE_L2)
+            .value("MULTIPLE_LABLE_SPACE_SO3", MULTIPLE_LABLE_SPACE_SO3)
+            .value("MULTIPLE_LABLE_SPACE_SE3", MULTIPLE_LABLE_SPACE_SE3)
+            .value("MULTIPLE_LABLE_SPACE_GEODIST", MULTIPLE_LABLE_SPACE_GEODIST)
+            .export_values();
 }
