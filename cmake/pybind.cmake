48e19bd4f4cfa18583e7c9e4fafccdc20343f84b
paul0noah <38716946+paul0noah@users.noreply.github.com>
2025-10-11 11:55:43 +0200

    Bump pybind

--- cmake/pybind.cmake
+++ cmake/pybind.cmake
@@ -3,6 +3,6 @@ message("Retrieving pybind11...")
 FetchContent_Declare(
     pybind
     GIT_REPOSITORY https://github.com/pybind/pybind11.git
-    GIT_TAG 6d22dba
+    GIT_TAG v3.0.0
 )
 FetchContent_MakeAvailable(pybind)
