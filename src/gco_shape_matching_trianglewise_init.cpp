3617ad887921b28d99604c4b778b6f2e6d19d023
Paul Roetzer <paul.roetzer@uni-bonn.de>
2025-11-17 20:39:53 +0100

    Upd init mode 6

--- src/gco_shape_matching_trianglewise_init.cpp
+++ src/gco_shape_matching_trianglewise_init.cpp
@@ -121,9 +121,9 @@ void GCOSM::triangleWiseInit(TriangleWiseOpts& opts,
             Eigen::MatrixXd featDifflr(VXlr.rows(), VYlr.rows());
             for (int i = 0; i < VXlr.rows(); i++) {
                 for (int j = 0; j < VYlr.rows(); j++) {
-                    featDifflr(i, j) = 0.5 * perVertexFeatureDifference(X_lr_2_hr(i, 0), Y_lr_2_hr(j, 0));
+                    featDifflr(i, j) = 0.9 * perVertexFeatureDifference(X_lr_2_hr(i, 0), Y_lr_2_hr(j, 0));
                     for (int n = 1; n < nn; n++) {
-                        featDifflr(i, j) += 0.05 * perVertexFeatureDifference(X_lr_2_hr(i, n), Y_lr_2_hr(j, n));;
+                        featDifflr(i, j) += 0.01 * perVertexFeatureDifference(X_lr_2_hr(i, n), Y_lr_2_hr(j, n));;
                     }
 
                 }
@@ -134,22 +134,148 @@ void GCOSM::triangleWiseInit(TriangleWiseOpts& opts,
             smGCO.updatePrefix("[GCOSM - INIT] ");
             TriangleWiseOpts optsCopy = opts;
             optsCopy.setInitialLables = 4;
+            smGCO.setMaxIter(20);
             const auto out = smGCO.triangleWise(optsCopy);
             Eigen::MatrixXi p2plr = std::get<1>(out);
-            Eigen::MatrixXi p2phr = p2plr;
-            for (int i = 0; i < p2phr.rows(); i++) {
-                p2phr.row(i) << X_lr_2_hr(p2plr(i, 0)), Y_lr_2_hr(p2plr(i, 1));
+            Eigen::MatrixXi p2plrRaw = std::get<3>(out);
+
+            PRINT_SMGCO(" init: matching extractioin");
+            std::vector<std::vector<int>> vertexMatches(VXlr.rows());
+            for (int i = 0; i < p2plrRaw.rows(); i++) {
+                vertexMatches[p2plrRaw(i, 0)].push_back(Y_lr_2_hr(p2plrRaw(i, 1)));
             }
-
-
-            Eigen::MatrixXf GeoDistXFeat = GeoDistX(Eigen::all, X_lr_2_hr.col(0));
-            std::cout << "check costmodes here" << std::endl;
-            Eigen::MatrixXf GeoDistYFeat = extraSmooth.geoDistY(Eigen::all, p2phr.col(1));
+            Eigen::MatrixXf maxGeoDist(VXlr.rows(), 1);
+            maxGeoDist.setZero();
+            for (int i = 0; i < VXlr.rows(); i++) {
+                for (const auto& targetVertex1 : vertexMatches[i]) {
+                    for (const auto& targetVertex2 : vertexMatches[i]) {
+                        maxGeoDist(i) = std::max(maxGeoDist(i), extraSmooth.geoDistY(targetVertex1, targetVertex2));
+                    }
+                }
+            }
             Eigen::MatrixXf LX, LY;
             igl::edge_lengths(VX, FX, LX);
             igl::edge_lengths(VY, FY, LY);
-            const float threshX = 3 * utils::median(LX);
-            const float threshY = 3 * utils::median(LY);
+            const float threshX = 20 * utils::median(LX);
+            const float threshY = 20 * utils::median(LY);
+
+            Eigen::MatrixXi p2phr = p2plr;
+            const float matchingDistThreshold = LY.maxCoeff();
+            int idx = 0;
+            for (int i = 0; i < p2phr.rows(); i++) {
+                if (maxGeoDist(i) >= matchingDistThreshold) {
+                    continue;
+                }
+                const int vertexX =  X_lr_2_hr(p2plr(i, 0));
+                const int vertexY =  Y_lr_2_hr(p2plr(i, 1));
+
+                p2phr.row(idx) << vertexX, vertexY;
+                idx++;
+            }
+            p2phr.conservativeResize(idx, 2);
+            PRINT_SMGCO(" init: kept " << p2phr.rows() << " of " << VXlr.rows()<< " matches from lowres")
+
+
+
+            // find closest matched vertices (on x) for each vertex of X
+            const int numNNMatchedPoints = 5;
+            const Eigen::MatrixXf AllPoints     = GeoDistX(Eigen::all, p2phr.col(0));
+            const Eigen::MatrixXf MatchedPoints = GeoDistX(p2phr.col(0), p2phr.col(0));
+            Eigen::MatrixXi closestMatchedPoints;
+            utils::knnsearch(AllPoints, MatchedPoints, closestMatchedPoints, numNNMatchedPoints);
+
+            PRINT_SMGCO(" init: new cost computation");
+            // for each vertex of X. find the best vertex on Y while using lowres matching
+            Eigen::MatrixXf newCost(VX.rows(), VY.rows()); newCost.setZero();
+            Eigen::MatrixXi interpolatedMatching(VX.rows(), 2);
+            const Eigen::MatrixXf& GeoDistY = extraSmooth.geoDistY;
+            for (int i = 0; i < VX.rows(); i++) {
+
+                // normalise with maxgeodist of closes match vertices
+                float maxGeodistBetweenMatchedOnX = 0.0f, maxGeodistBetweenMatchedOnY = 0.0f;
+                for (int k = 0; k < numNNMatchedPoints; k++) {
+                    const int matchedIdxK = closestMatchedPoints(i, k);
+                    for (int kk = 0; kk < numNNMatchedPoints; kk++) {
+                        const int matchedIdxKK = closestMatchedPoints(i, kk);
+                        maxGeodistBetweenMatchedOnX = std::max(maxGeodistBetweenMatchedOnX, GeoDistX(p2phr(matchedIdxK, 0), p2phr(matchedIdxKK, 0)));
+                        maxGeodistBetweenMatchedOnY = std::max(maxGeodistBetweenMatchedOnY, GeoDistY(p2phr(matchedIdxK, 1), p2phr(matchedIdxKK, 1)));
+                    }
+                }
+
+                // compute on X
+                Eigen::MatrixXf distancesX(1, numNNMatchedPoints);
+                for (int k = 0; k < numNNMatchedPoints; k++) {
+                    const int matchedIdxK = closestMatchedPoints(i, k);
+                    distancesX(0, k) = GeoDistX(i, p2phr(matchedIdxK, 0)) / maxGeodistBetweenMatchedOnX;
+                }
+
+                // find best matching on Y
+                float bestCost = std::numeric_limits<float>::infinity();
+                int bestIndex = 0;
+                for (int j = 0; j < VY.rows(); j++) {
+                    float maxGeoDistToMatched = 0.0;
+                    for (int k = 0; k < numNNMatchedPoints; k++) {
+                        const int matchedIdxK = closestMatchedPoints(i, k);
+                        maxGeoDistToMatched = std::max(maxGeoDistToMatched, GeoDistY(j, p2phr(matchedIdxK, 1)));
+                    }
+
+                    float cost = 0;
+                    for (int k = 0; k < numNNMatchedPoints; k++) {
+                        const int matchedIdxK = closestMatchedPoints(i, k);
+                        const float distanceKonY = GeoDistY(j, p2phr(matchedIdxK, 1)) / maxGeodistBetweenMatchedOnY;
+                        cost += std::abs(distanceKonY - distancesX(0, k));
+                    }
+                    newCost(i, j) = cost * maxGeodistBetweenMatchedOnX * maxGeodistBetweenMatchedOnY;
+                    if (maxGeoDistToMatched > maxGeodistBetweenMatchedOnY) {
+                        continue;
+                    }
+                    if (cost < bestCost) {
+                        bestCost = cost;
+                        bestIndex = j;
+                    }
+                }
+
+                interpolatedMatching(i, 0) = i;
+                interpolatedMatching(i, 1) = bestIndex;
+            }
+
+
+
+            /*minLables = interpolatedMatching;
+            std::cout << minLables.rows() << minLables.cols() << std::endl;
+            return;*/
+
+
+            std::cout << "check costmodes here" << std::endl;
+            /*
+             newCost.setConstant(1);
+             for (int i = 0; i < interpolatedMatching.rows(); i++)
+                 newCost(interpolatedMatching(i, 0), interpolatedMatching(i, 1)) = 0;
+            #if defined(_OPENMP)
+            #pragma omp parallel for
+            #endif
+            for (int i = 0; i < FX.rows(); i++) {
+                float bestEnergy = std::numeric_limits<float>::infinity();
+                for (unsigned long l = 0; l < numLables; l++) {
+                    float cost = 0;
+                    for (int j = 0; j < 3; j++) {
+                        cost += newCost(FX(i, j), extraSmooth.LableFY(l, j));
+                    }
+                    if (cost < bestEnergy) {
+                        bestEnergy = cost;
+                        minLables(i) = l;
+                    }
+                }
+            }
+
+            for (int i = 0; i < FX.rows(); i++) {
+                gc->setLabel(i, minLables(i) + i * numLables);
+            }
+            return;*/
+
+            /*
+            Eigen::MatrixXf GeoDistXFeat = GeoDistX(Eigen::all, p2phr.col(0));
+            Eigen::MatrixXf GeoDistYFeat = extraSmooth.geoDistY(Eigen::all, p2phr.col(1));
             std::cout << threshX << ", " << threshY << std::endl;
             for (int i = 0; i < VX.rows(); i++) {
                 for (int j = 0; j < GeoDistXFeat.cols(); j++) {
@@ -163,15 +289,15 @@ void GCOSM::triangleWiseInit(TriangleWiseOpts& opts,
                     GeoDistYFeat(i, j) /= threshY;
                 }
             }
-            Eigen::MatrixXf featDiffNew(VX.rows(), VY.rows());
-            featDiffNew = 0.1 * perVertexFeatureDifference.cast<float>();
+
+            newCost = 0.1 * perVertexFeatureDifference.cast<float>();
 
             for (int i = 0; i < VX.rows(); i++) {
                 for (int j = 0; j < VY.rows(); j++) {
                     float cost = (GeoDistXFeat.row(i) - GeoDistYFeat.row(j)).cwiseAbs().sum();
-                    featDiffNew(i, j) += cost;
+                    newCost(i, j) += cost;
                 }
-            }
+            }*/
             //Eigen::MatrixXi t;
             //return std::make_tuple(optimisationTime, p2phr, t, p2phr, t);
             #if defined(_OPENMP)
@@ -187,7 +313,7 @@ void GCOSM::triangleWiseInit(TriangleWiseOpts& opts,
                 for (unsigned long l = iterStart; l < numLables; l++) { // this loop is lable of the center triangle
                     double sum = 0;
                     for (int j = 0; j < 3; j++) {
-                        sum += featDiffNew(FX(i, j), extraSmooth.LableFY(l, j));
+                        sum += newCost(FX(i, j), extraSmooth.LableFY(l, j));
                     }
 
                     // find best cost for neighbouring lables that are in zero distance of the current lable l
@@ -232,7 +358,7 @@ void GCOSM::triangleWiseInit(TriangleWiseOpts& opts,
                             }
                             const bool remainingVertexNotTheSame = setInitialLables == 3 ? remainingVertex1 != remainingVertex2 : true;
                             if (remainingVertexNotTheSame) {
-                                const double cost = featDiffNew(FX(adjTri, remainingVertexIdx), extraSmooth.LableFY(ll, remainingVertexIdx));
+                                const double cost = newCost(FX(adjTri, remainingVertexIdx), extraSmooth.LableFY(ll, remainingVertexIdx));
                                 if (cost < bestCost) {
                                     bestCost = cost;
                                     if (!output) {
